d6e7fdd1e391eff393301f0547aac23630dc3200
fredrikwide <fredrik.widegren@gmail.com>
2020-11-23 12:01:07 +0100

    new branch main

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default firebaseConfig = {
+    apiKey: "AIzaSyDQhaDC0C9EwurjYbGMlmEMdq0UOqdRJ1U",
+    authDomain: "svelte-todos-fedef.firebaseapp.com",
+    databaseURL: "https://svelte-todos-fedef.firebaseio.com",
+    projectId: "svelte-todos-fedef",
+    storageBucket: "svelte-todos-fedef.appspot.com",
+    messagingSenderId: "749006298293",
+    appId: "1:749006298293:web:f47d4b74c4ca6afa4772fc",
+    measurementId: "G-GNYMFJ3RQS"
+};
